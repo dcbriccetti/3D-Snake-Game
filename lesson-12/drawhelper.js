f1c70df4d50d53bd3e56add2b927ae67936fa15b
Dave Briccetti <daveb@davebsoft.com>
2020-03-18 19:08:59 -0700

    Start lesson 12

---
+++ lesson-12/drawhelper.js
@@ -0,0 +1,19 @@
+/**
+ * Returns an “at” function which uses the provided p5 object.
+ * @param p5 the p5 instance on which to invoke p5 methods
+ */
+function makeAt(p5) {
+    /**
+     * Translates by the specified x, y, z distances, then calls the provided function. Wraps in a push/pop.
+     * @param point The x, y, z distances by which to translate
+     * @param fn The function to call after translating
+     */
+    function at(point, fn) {
+        p5.push();
+        p5.translate(...point);
+        fn();
+        p5.pop();
+    }
+    return at;
+}
+//# sourceMappingURL=drawhelper.js.map
