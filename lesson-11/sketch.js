67f45501b46a2dc97d8f076eba5012e3c446be66
Dave Briccetti <daveb@davebsoft.com>
2020-03-18 18:44:27 -0700

    Complete Lesson 11

--- lesson-11/sketch.js
+++ lesson-11/sketch.js
@@ -1,29 +1,36 @@
 // 3D Snake Game
-// Lesson Ten: Adding User Interface Sliders
+// Lesson Eleven: Adding Multiple Snakes
 new p5(p => {
     const STARTING_NUM_SEGMENTS = 3;
     const SPEEDUP_FACTOR = 3;
+    const SNAKE_RGBS = [
+        [0, 255, 0],
+        [0, 255, 255],
+        [0, 0, 255],
+        [255, 0, 255],
+        [255, 0, 0],
+        [255, 128, 0],
+        [255, 255, 0],
+    ];
     let cellsPerDimension = 11;
-    let msPerMove = 1000;
-    let msPerAutoMove = 100;
+    let msPerMove;
     let food;
     let foodImage;
-    let direction;
-    let segments;
     let keyMappings;
     let arenaWidth;
     let cellWidth;
-    let zeroVector;
     let nextMoveTime;
-    let autoDriving = false;
     let rightmostCellCenter;
+    let at;
+    let numSnakes;
+    let snakes;
     p.preload = () => {
         foodImage = p.loadImage('apple.png');
     };
     p.setup = () => {
+        at = makeAt(p);
         const len = p.min(p.windowWidth - 10, p.windowHeight - 50);
         p.createCanvas(len, len, p.WEBGL);
-        zeroVector = p.createVector(0, 0, 0);
         arenaWidth = p.round(p.width * 0.6);
         resizeFromSlider();
         mapKeys();
@@ -31,34 +38,32 @@ new p5(p => {
         createControls();
     };
     p.draw = () => {
-        if (p.millis() > nextMoveTime) {
-            if (autoDriving)
-                autoSetDirection();
-            moveSnake();
-            const ms = autoDriving ? msPerAutoMove : msPerMove;
-            nextMoveTime += p.keyIsDown(p.SHIFT) ? ms / SPEEDUP_FACTOR : ms;
+        const now = p.millis();
+        const msPastNextMoveTime = now - nextMoveTime;
+        if (msPastNextMoveTime > 0) {
+            snakes.forEach(s => {
+                if (s.autoDriving)
+                    s.autoSetDirection(snakes, food);
+                s.move(snakes, food, () => s.die(), (foundFood) => food = newFoodPosition());
+            });
+            const adjustedMs = p.keyIsDown(p.SHIFT) ? msPerMove / SPEEDUP_FACTOR : msPerMove;
+            nextMoveTime = p.max(now, now + adjustedMs - msPastNextMoveTime);
         }
         positionCamera();
         p.background(255);
         p.smooth();
         drawArena();
-        drawSnake();
+        snakes.forEach((s, i) => s.draw(SNAKE_RGBS[i % SNAKE_RGBS.length], drawReferenceStructures));
         drawFood();
     };
     p.keyPressed = () => {
-        if (p.key === 'a') {
-            if (autoDriving = !autoDriving)
-                nextMoveTime = p.millis();
-        }
-        else {
-            const requestedDir = keyMappings[p.key];
-            if (requestedDir) {
-                const oppositeOfCurrentDir = p5.Vector.mult(direction, -1);
-                if (!requestedDir.equals(oppositeOfCurrentDir)) {
-                    direction = requestedDir;
-                    if (!nextMoveTime)
-                        nextMoveTime = p.millis();
-                }
+        const requestedDir = keyMappings[p.key];
+        if (requestedDir) {
+            const oppositeOfCurrentDir = p5.Vector.mult(snakes[0].direction, -1);
+            if (!requestedDir.equals(oppositeOfCurrentDir)) {
+                snakes[0].direction = requestedDir;
+                if (!nextMoveTime)
+                    nextMoveTime = p.millis();
             }
         }
     };
@@ -76,18 +81,19 @@ new p5(p => {
             resizeFromSlider();
             setUpState();
         });
-        const sliderManualSpeed = p.select('#manualSpeed');
+        const sliderManualSpeed = p.select('#speed');
         function setMsPerMoveFromSlider() {
-            msPerMove = p.map(sliderManualSpeed.value(), 1, 50, 5000, 100);
+            msPerMove = p.map(sliderManualSpeed.value(), 1, 50, 3000, 0);
         }
         sliderManualSpeed.changed(() => setMsPerMoveFromSlider());
         setMsPerMoveFromSlider();
-        const sliderAutoSpeed = p.select('#autoSpeed');
-        function setMsPerMoveFromAutoSlider() {
-            msPerAutoMove = p.map(sliderAutoSpeed.value(), 1, 50, 1000, 0);
+        const sliderNumSnakes = p.select('#numSnakes');
+        function setNumSnakesFromAutoSlider() {
+            numSnakes = sliderNumSnakes.value();
+            setUpState();
         }
-        sliderAutoSpeed.changed(() => setMsPerMoveFromAutoSlider());
-        setMsPerMoveFromAutoSlider();
+        sliderNumSnakes.changed(() => setNumSnakesFromAutoSlider());
+        setNumSnakesFromAutoSlider();
     }
     function resizeFromSlider() {
         cellWidth = p.round(arenaWidth / cellsPerDimension);
@@ -105,6 +111,8 @@ new p5(p => {
         keyMappings = {
             'w': away,
             's': towards,
+            'Home': away,
+            'End': towards,
             'ArrowLeft': left,
             'ArrowRight': right,
             'ArrowUp': up,
@@ -112,71 +120,13 @@ new p5(p => {
         };
     }
     function setUpState() {
-        direction = p.createVector(0, 0, 0);
+        snakes = Array.from({ length: numSnakes }, (v, i) => new Snake(p, i, arenaWidth, () => cellWidth, cellsPerDimension, STARTING_NUM_SEGMENTS));
         food = newFoodPosition();
-        segments = Array.from({ length: STARTING_NUM_SEGMENTS }, (v, i) => p.createVector(-i * cellWidth, 0, 0));
     }
     function newFoodPosition() {
         const m = cellsRightOfCenter();
         const c = () => p.round(p.random(-m, m)) * cellWidth;
         return p.createVector(c(), c(), c());
-    }
-    function moveSnake() {
-        if (autoDriving || !direction.equals(zeroVector)) {
-            const newHeadPos = p5.Vector.add(segments[0], p5.Vector.mult(direction, cellWidth));
-            if (collides(newHeadPos)) {
-                setUpState();
-            }
-            else {
-                if (newHeadPos.equals(food))
-                    food = newFoodPosition();
-                else
-                    segments.pop(); // Discard last
-                segments.unshift(newHeadPos); // Put new head on front
-            }
-        }
-    }
-    function collides(pos) {
-        const inBounds = pos.array().every(coord => Math.abs(coord) < arenaWidth / 2);
-        const collidesWithSelf = segments.some((segment, i) => i > 0 && segment.equals(pos));
-        return collidesWithSelf || !inBounds;
-    }
-    function autoSetDirection() {
-        const head = segments[0];
-        const toFoodAxisDistances = p5.Vector.sub(food, head).array();
-        let newDir;
-        const validDirs = validMoveDirections(head);
-        for (let i = 0; i < 3; i++) {
-            const d = toFoodAxisDistances[i];
-            const a = [0, 0, 0];
-            a[i] = d / Math.abs(d); // -1, 0, or 1
-            const candidateDir = p.createVector(...a);
-            if (validDirs.some(d => d.equals(candidateDir))) {
-                newDir = candidateDir;
-                break;
-            }
-        }
-        if (newDir)
-            direction = newDir;
-        else {
-            if (validDirs.length) {
-                direction = p.random(validDirs);
-            }
-        }
-    }
-    function validMoveDirections(head) {
-        const validDirs = [];
-        [-1, 1].forEach(n => {
-            for (let axis = 0; axis < 3; axis++) {
-                const dirArray = [0, 0, 0];
-                dirArray[axis] = n;
-                const candidateDir = p.createVector(...dirArray);
-                const candidatePos = p5.Vector.add(head, p5.Vector.mult(candidateDir, cellWidth));
-                if (!collides(candidatePos))
-                    validDirs.push(candidateDir);
-            }
-        });
-        return validDirs;
     }
     function drawArena() {
         p.stroke('gray');
@@ -197,17 +147,6 @@ new p5(p => {
                     p.line(v, s, 0, v, l, 0);
                 }
             });
-        });
-    }
-    function drawSnake() {
-        const segmentWidth = cellWidth * 0.9;
-        segments.forEach((segment, i) => {
-            p.stroke('gray');
-            p.fill(i === 0 ? 255 : 0, 255, 0, 70);
-            at(segment.array(), () => p.box(p.map(i, 0, segments.length, segmentWidth, segmentWidth * 0.5)));
-            p.stroke(0, 255, 0);
-            p.fill(0, 255, 0, 60);
-            drawReferenceStructures(segments[0], segmentWidth);
         });
     }
     function drawFood() {
@@ -232,12 +171,6 @@ new p5(p => {
         at([l, y, z], () => p.box(f, w, w));
         at([x, l, z], () => p.box(w, f, w));
         at([x, y, s], () => p.box(w, w, f));
-    }
-    function at(point, fn) {
-        p.push();
-        p.translate(...point);
-        fn();
-        p.pop();
     }
 });
 //# sourceMappingURL=sketch.js.map
